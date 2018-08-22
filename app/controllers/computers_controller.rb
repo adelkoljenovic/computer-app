11fc9e0e3497301ec012cd2b1c2a07abeece7b96
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-08-22 19:40:06 -0400

    create show action & show.html.erb file

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -1,14 +1,14 @@
 class ComputersController < ApplicationController
   def index
     @computers = Computer.all
-  
   end
 
   def show
-  
+    @computer = Computer.find_by(id: params[:id])
   end
 
   def new
+    
   
   end
 
